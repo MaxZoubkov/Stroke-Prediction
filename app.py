f7aee1895c735ff26f844d192617b36ceb2ce91b
MaxZoubkov <MaxEZoubkov@gmail.com>
2021-12-06 16:45:31 -0500

    Added code to output the result (commented out until model is usable), added about.html

--- app.py
+++ app.py
@@ -7,37 +7,47 @@ import logging
 
 app = Flask(__name__, template_folder='templates')
 
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
 
     if flask.request.method == 'POST':
-        #app.logger.info(flask.request.form)    # useful to see what is submitted with the form
+        app.logger.info(flask.request.form)    # useful to see what is submitted with the form
         
-        input_list = flask.request.form
-        # app.logger.info(X)
-        # gender = flask.request.form['gender']
-        # age = flask.request.form['age']
-        # hypertension = flask.request.form['hypertension']
-        # heart_disease = flask.request.form['heart_disease']
-        # ever_married = flask.request.form['ever_married']
-        # work_type = flask.request.form['work_type']
-        # residence_type = flask.request.form['residence_type']
-        # avg_glucose_level = flask.request.form['avg_glucose_level']
-        # bmi = flask.request.form['bmi']
-        # smoking_status = flask.request.form['smoking_status']
+        input_dict = flask.request.form.to_dict()
+        app.logger.info(input_dict)
+
+        with open('model/model.sav', 'rb') as f:
+            model = pickle.load(f)
+
+        df = pd.DataFrame(input_dict, index=[0])
+        app.logger.info(df)
 
         # Feeding the model and outputting to the page will be here
+        # Assuming the model has a member element "model.feature_list"
+        # df = df[model.feature_list]
+        # y_pred = model.predict(df)
+        # probability = model.predict_proba(df)
+        # result = y_pred[i]
+        #if result is 0:
+            #result = "High risk of stroke!"
+        #else:
+            #result = "Low risk of stroke."
+        #probability = probability[0]
 
         result = "some result"  # change to stroke prediction
+        probability = "some probability"    # change to stroke probability
 
-        return render_template('main.html', input_list=input_list, result=result) # placeholder template
+        return render_template('main.html', result=result, probability=probability) # placeholder template
 
 
 @app.route('/about')
 def about():
-    return render_template('result.html') #change to about.html
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
