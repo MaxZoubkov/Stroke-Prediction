1d16819f035965551aea04636ef1a50f7e6df690
MaxZoubkov <MaxEZoubkov@gmail.com>
2021-12-06 21:23:20 -0500

    Code to output result now takes into account dummy columns used to create the model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import Flask, url_for, render_template
 import pickle
 import pandas as pd
+import re
 import logging
 
 
@@ -24,19 +25,37 @@ def main():
         with open('model/model.sav', 'rb') as f:
             model = pickle.load(f)
 
+
         df = pd.DataFrame(input_dict, index=[0])
         app.logger.info(df)
 
         # Feeding the model and outputting to the page will be here
         # Assuming the model has a member element "model.feature_list"
-        # df = df[model.feature_list]
+        # input_list = []
+        # for key, value in input_dict:
+        #   input_list.append(key + '_' + value)
+        # feature_list = model.feature_list
+        # 
+        # tuple = []
+        # for feature in feature_list:
+        #   if feature in input_list:
+        #       tuple.append(1)
+        #   else:
+        #       tuple.append(0)
+        # model_df = pd.DataFrame(columns=[feature_list])
+        # for s in input_list:
+        #   model_df[s] = 1
+
+
+
+
         # y_pred = model.predict(df)
         # probability = model.predict_proba(df)
         # result = y_pred[i]
-        #if result is 0:
-            #result = "High risk of stroke!"
-        #else:
-            #result = "Low risk of stroke."
+        # if result is 0:
+        #   result = "High risk of stroke!"
+        # else:
+        #   result = "Low risk of stroke."
         #probability = probability[0]
 
         result = "some result"  # change to stroke prediction
